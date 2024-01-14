ed1d099869351d9887df6f5fdeb3023bcc0030b9
sanghvirachna <rachnasanghvi123@gmail.com>
2024-01-15 00:27:33 +0530

    some ui changes

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import './Share.css'
 const Share = () => {
   return (
     <div>
-      Secrets Room
+      <header>
+        <div className='heading'>
+          <p>Your identity is completely protected </p>
+          <img src="https://cdn-icons-png.flaticon.com/256/2592/2592317.png" alt="secure"></img>
+        </div>
+        <img src="https://cdn-icons-png.flaticon.com/256/149/149071.png" alt="user" className='user'></img>
+      </header>
     </div>
   )
 }
